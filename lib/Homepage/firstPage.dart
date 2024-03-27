72ee8c978c3407e6fcb1c6762b0088597b6b9239
ManifestJosh <okeugochijoshua@gmail.com>
2024-03-27 05:05:04 +0100

    added introscreen

--- lib/Homepage/firstPage.dart
+++ lib/Homepage/firstPage.dart
@@ -1,10 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_payment_platform/Homepage/homepage.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class FirstPage extends StatelessWidget {
-  const FirstPage({super.key});
+class OnBoardingScreen extends StatefulWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  PageController _controller = PageController();
+
+  bool onLastPage = false;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: Stack(
+      children: [
+        PageView(
+          controller: _controller,
+          onPageChanged: (index) {
+            setState(() {
+              onLastPage = (index == 2);
+            });
+          },
+          children: [
+            Container(color: Colors.white),
+            Container(color: Colors.blue.shade200),
+            Container(color: Colors.blue.shade400)
+          ],
+        ),
+        Container(
+          alignment: Alignment(0, 0.75),
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            GestureDetector(
+              onTap: () {
+                _controller.jumpToPage(2);
+              },
+              child: Text('Skip'),
+            ),
+            SmoothPageIndicator(controller: _controller, count: 3),
+            onLastPage
+                ? GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return Homepage();
+                      }));
+                    },
+                    child: Text('Let me in'),
+                  )
+                : GestureDetector(
+                    onTap: () {
+                      _controller.nextPage(
+                        duration: Duration(milliseconds: 500),
+                        curve: Curves.easeIn,
+                      );
+                    },
+                    child: Text('Next'),
+                  ),
+          ]),
+        )
+      ],
+    ));
   }
 }
--- lib/Homepage/homepage.dart
+++ lib/Homepage/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:mobile_payment_platform/styles.dart';
 
 class Homepage extends StatelessWidget {
@@ -7,68 +8,124 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String username = "Blvck";
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "W4LLET",
-          style: headingTextStyle,
-        ),
-        centerTitle: true,
-        leading: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Image.asset(
-            'assets/logos.png',
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "W4LLET",
+            style: headingTextStyle,
+          ),
+          centerTitle: true,
+          leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset(
+              'assets/logos.png',
+            ),
           ),
         ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 32.0),
-        child: Row(
-          children: <Widget>[
-            Text(
-              "Welcome,$username",
-              style: accountHeadTextStyle,
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    "Welcome, $username",
+                    style: accountHeadTextStyle,
+                  ),
+                  const Spacer(),
+                  const Icon(
+                    Icons.notifications,
+                    color: Color.fromARGB(255, 165, 212, 249),
+                    size: 30,
+                  )
+                ],
+              ),
             ),
-            const Spacer(),
-            const Icon(
-              Icons.notifications,
-              color: Color.fromARGB(255, 16, 33, 46),
-              size: 30,
+            GestureDetector(
+              child: SizedBox(
+                height: 250,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    HorizontalTile(),
+                    SizedBox(
+                      width: 12,
+                    ),
+                    HorizontalTile(),
+                    SizedBox(
+                      width: 12,
+                    ),
+                    HorizontalTile(),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 4,
+                itemBuilder: (BuildContext context, index) {
+                  return Container(
+                    child: ListTile(),
+                    margin: EdgeInsets.all(16.0),
+                    height: 100,
+                    decoration: BoxDecoration(
+                        border: Border.all(),
+                        borderRadius: BorderRadius.all(Radius.circular(16.0))),
+                  );
+                },
+              ),
             )
           ],
         ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.blue.shade100,
-        shape: const CircularNotchedRectangle(),
-        child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.home),
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: Icon(Icons.search),
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: Icon(Icons.payment),
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: Icon(Icons.menu),
-                onPressed: () {},
-              ),
-            ]),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.blue.shade100,
-        shape: const CircleBorder(eccentricity: 0.7),
-        onPressed: () {},
-        child: Icon(Icons.add),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.blue.shade100,
+          shape: const CircularNotchedRectangle(),
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.home),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(Icons.payment),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(Icons.menu),
+                  onPressed: () {},
+                ),
+              ]),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.blue.shade100,
+          shape: const CircleBorder(eccentricity: 0.7),
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      );
+    });
+  }
+}
+
+class HorizontalTile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 350,
+      margin: EdgeInsets.symmetric(horizontal: 16.0),
+      decoration: BoxDecoration(
+          border: Border.all(),
+          borderRadius: BorderRadius.all(Radius.circular(20))),
     );
   }
 }
